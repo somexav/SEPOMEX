0a6be32f3a42bd3d8198c7a07a06a8d6b1fb0599
Javier Martinez <somexav@comunidad.unam.mx>
2022-05-01 17:50:35 -0500

    Add files via upload

--- API/app.py
+++ API/app.py
@@ -25,6 +25,91 @@ def index():
 
     return "<h1> HELLO </h1>"
 
+#######-----------BUSQUEDA DE  COLONIAS POR CP
+@app.route('/busqueda/colonias/CP:<CP>', methods = ['GET'])
+def colonias_seacrh(CP):
+    try:
+        cur = mysql.connection.cursor()
+        query = "SELECT D_ASENTA FROM sepomex WHERE D_CODIGO ='{}'".format(CP)
+        cur.execute(query)
+        results =  cur.fetchall()
+        lista  = []
+        i = 0
+        for fila in results:
+            i = i + 1
+            row = {'No: {}'.format(i):fila[0]}
+            
+            lista.append(row)
+        return jsonify({'Colonias por CP':lista})
+    except Exception as exp:
+        return jsonify('Mensaje', 'ERROR, no hay colonias en ese CP')
+
+
+#######-----------Despliegue de MUNICIPIOS por ESTADO
+
+@app.route('/busqueda/municipios/<name>', methods = ['GET'])
+def Municipios_Estado(name):
+    try:
+        cur = mysql.connection.cursor()
+        query = "SELECT C_ESTADO FROM estado WHERE D_ESTADO ='{}'".format(name)
+        cur.execute(query)
+        result =  cur.fetchone() #En estado se espera resultado unico
+        if result != None: 
+            codigo = result[0]
+            #row = {'ID':result[0]}
+            #return jsonify({'Estado':result, 'Mensaje':'Estado encontrado'})
+            subquery = "SELECT D_ASENTA FROM sepomex WHERE C_ESTADO ='{}'".format(codigo)
+            cur.execute(subquery)
+            results =  cur.fetchall()
+            lista  = []
+            i=0
+            for fila in results:
+                i = i + 1
+                row = {'No: {}'.format(i):fila[0]}
+                lista.append(row)
+            return jsonify({'Municipios encontrados en {}'.format(name):lista})
+        else: 
+            return jsonify({'Mensaje':'Estado {} encontrado '.format(name)})
+    except Exception as exp:
+        return jsonify('Mensaje', 'ERROR DESCONOCIDO')
+
+#######-----------Despliegue de COLONIAS por MUNICIPIO
+
+@app.route('/busqueda/colonias/<name>', methods = ['GET'])
+def Colonias_Municipio(name):
+    try:
+        cur = mysql.connection.cursor()
+        query = "SELECT C_MUNICIPIO FROM municipio WHERE D_MUNICIPIO ='{}'".format(name)
+        cur.execute(query)
+        result =  cur.fetchone() #En estado se espera resultado unico
+        if result != None: 
+            codigo = result[0]
+            #row = {'ID':result[0]}
+            #return jsonify({'Municipio':result, 'Mensaje':'Municipio encontrado'})
+            subquery = "SELECT SEPOMEX_ID,D_ASENTA,C_ESTADO FROM sepomex WHERE C_MUNICIPIO ='{}'".format(codigo)
+            
+            cur.execute(subquery)
+            results =  cur.fetchall()
+            lista  = []
+            i = 0
+            for fila in results:
+                i = i+1
+                edos_query = "SELECT D_ESTADO FROM estado WHERE C_ESTADO ={}".format(fila[2])
+                cur.execute(edos_query)
+                edo = cur.fetchone()
+                row = {'ID':fila[0],'Edo:':edo[0],'Colonia {}'.format(i):fila[1]}
+                lista.append(row)
+            return jsonify({'Colonias encontrados en el Municipio {}'.format(name):lista})
+        else: 
+            return jsonify({'Mensaje':'Municipio NO encontrado '})
+    except Exception as exp:
+        return jsonify('Mensaje', 'ERROR DESCONOCIDO')
+
+
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
+#######-----------Despliegue de ESTADOS
 
 @app.route('/estados', methods = ['GET'])
 def listar_estados():
@@ -37,11 +122,11 @@ def listar_estados():
         for fila in results:
             row = {'ID':fila[0],'Estado':fila[1]}
             lista.append(row)
-
-        return jsonify({'Estados':results, 'Mensaje':'Estados Listados'})
+        return jsonify({'Estados Listados':lista})
     except Exception as exp:
         return jsonify('Mensaje', 'ERROR')
 
+#######-----------Despliegue de por codigo
 
 @app.route('/estados/<codigo>', methods = ['GET'])
 def buscar_estado(codigo):
@@ -53,11 +138,13 @@ def buscar_estado(codigo):
         
         if result != None:
             row = {'ID':result[0],'Estado':result[1]}
-            return jsonify({'Estado':result, 'Mensaje':'Estado encontrado'})
+            return jsonify({'Estado Encontrado':row})
         else: 
             return jsonify({'Mensaje':'Estado NO encontrado '})
     except Exception as exp:
         return jsonify('Mensaje', 'ERROR')
+
+#---------------NO SE QUE PASA CON POST :( -------------------------------
 
 @app.route('/estados', methods = ['POST'])
 def registrar_estado():
@@ -65,7 +152,7 @@ def registrar_estado():
         #print(request.json)
         cur = mysql.connection.cursor()
         query = """INSERT INTO estado (C_ESTADO, D_ESTADO) 
-                    VALUES ({0},'{1}')""".format(request.json['C_ESTADO'],
+                    VALUES ({0},{1})""".format(request.json['C_ESTADO'],
                                                 request.json['D_ESTADO'])
         cur.execute(query)
         mysql.connection.commit()
